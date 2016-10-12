5e1de17422e4cfd49f284bebd7a22fdba1b7445d
Michael Kamau <tokamau@gmail.com>
2016-10-12 10:12:27 +0300

    Add doc_string

--- missing_number_src/missing_number.py
+++ missing_number_src/missing_number.py
@@ -1,5 +1,10 @@
-
 def find_missing(list1, list2):
+    """
+    Finds and returns  the element in either list1 or list2 but not both
+    :param list1
+    :param list2
+    :return: element
+    """
     set1 = set(list1)
     set2 = set(list2)
     diff = set1 ^ set2
